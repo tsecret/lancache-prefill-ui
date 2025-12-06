fd33b7fc35e02d9a573004ba0e1482d9bf50f75e
tsecret <timichfull@gmail.com>
2025-12-06 02:10:08 +0100

    cellid 66 as hotfix

--- apps/backend/src/clients/docker.ts
+++ apps/backend/src/clients/docker.ts
@@ -1,5 +1,5 @@
 import { getLogger } from "@logtape/logtape";
-import type { Container, Image } from "shared/types";
+import { Services, type Container, type Image } from "shared/types";
 import { getContainerSettingsFromTag } from "../utils";
 import Docker = require('dockerode');
 
@@ -95,7 +95,7 @@ class DockerService {
       {
         Image: tag,
         name: containerName,
-        Cmd: ['prefill'],
+        Cmd: ['prefill', tag.includes(Services.STEAM) ? '--cellid 66' : ''],
         Tty: true,
         HostConfig: {
           AutoRemove: autoremove,
